1c214c025a95ab9244a62327ebab464c4945edfe
Rodrigo Habib Gregori <rodrigo.gregori@gmail.com>
2015-12-03 22:07:41 -0200

    Corrigidos parênteses no bacteria.service

--- static/js/services/bacteria.service.js
+++ static/js/services/bacteria.service.js
@@ -5,48 +5,49 @@
 		.module('ccihApp')
 		.factory('BacteriaService', BacteriaService);
 
-		BacteriaService.$inject = ['$mdDialog','$http'];
+	BacteriaService.$inject = ['$mdDialog','$http'];
 
-		function BacteriaService ($mdDialog, $http) {
-			var service = {};
+	function BacteriaService ($mdDialog, $http) {
+		var service = {};
 
-			service.ShowForm = ShowForm;
-			service.SaveBacteria = SaveBacteria;
-			service.UpdateBacteria = UpdateBacteria;
-			service.DeleteBacteria = DeleteBacteria;
-			service.GetAllBacterias = GetAllBacterias;
-			service.GetBacteria = GetBacteria;
+		service.ShowForm = ShowForm;
+		service.SaveBacteria = SaveBacteria;
+		service.UpdateBacteria = UpdateBacteria;
+		service.DeleteBacteria = DeleteBacteria;
+		service.GetAllBacterias = GetAllBacterias;
+		service.GetBacteria = GetBacteria;
 
-			return service;
+		return service;
 
-			function ShowForm (event) {
-				return $mdDialog.show({
-					controller: BacteriaController,
-					templateUrl: 'views/bacteria.view.html',
-					parent: angular.element(document.body),
-					targetEvent: ev,
-					clickOutsiteToClose: false,
-				});
-			}
-
-			function SaveBacteria (bacteria) {
-				return $http.post($rootScope.urlBase + '/bacterias', bacteria);
-			}
-
-			function UpdateBacteria (bacteria) {
-				return $http.put($rootScope.urlBase + '/bacterias/' + bacteria.id, bacteria);
-			}
-
-			function DeleteBacteria (id) {
-				return $http.delete($rootScope.urlBase + '/bacterias/' + id);
-			}
-
-			function GetAllBacterias (bacteria) {
-				return $http.get($rootScope.urlBase + '/bacterias');
-			}
-
-			function GetBacteria (id) {
-				return $http.get($rootScope.urlBase + '/bacterias/' + id);
-			}
-
+		function ShowForm (event) {
+			return $mdDialog.show({
+				controller: BacteriaController,
+				templateUrl: 'views/bacteria.view.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsiteToClose: false,
+			});
 		}
+
+		function SaveBacteria (bacteria) {
+			return $http.post($rootScope.urlBase + '/bacterias', bacteria);
+		}
+
+		function UpdateBacteria (bacteria) {
+			return $http.put($rootScope.urlBase + '/bacterias/' + bacteria.id, bacteria);
+		}
+
+		function DeleteBacteria (id) {
+			return $http.delete($rootScope.urlBase + '/bacterias/' + id);
+		}
+
+		function GetAllBacterias (bacteria) {
+			return $http.get($rootScope.urlBase + '/bacterias');
+		}
+
+		function GetBacteria (id) {
+			return $http.get($rootScope.urlBase + '/bacterias/' + id);
+		}
+
+	}
+})();
